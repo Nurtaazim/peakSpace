7b4e6e5c381ccf167fd0ee97639e2db8ddeae159
mukhammed.codes <muhammedtoichubai@gmail.com>
2024-04-04 10:55:48 +0600

    inital commit

---
+++ src/main/java/peakspace/PeakSpaceB12Application.java
@@ -0,0 +1,13 @@
+package peakspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakSpaceB12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeakSpaceB12Application.class, args);
+    }
+
+}
---
+++ src/test/java/peakspace/PeakSpaceB12ApplicationTests.java
@@ -0,0 +1,13 @@
+package peakspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakSpaceB12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
