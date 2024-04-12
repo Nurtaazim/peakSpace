821d8a839591dad1dd46446d9bf3e4592bf6d441
Arstanbeekovvv <arstanbeekovvv@gmail.com>
2024-04-12 15:12:06 +0600

    auth with google(sign_in and sign_up)

--- src/main/java/peakspace/service/UserService.java
+++ src/main/java/peakspace/service/UserService.java
@@ -1,13 +1,10 @@
 package peakspace.service;
-
-import com.google.firebase.auth.FirebaseAuthException;
-import org.hibernate.type.descriptor.java.JdbcTimeJavaType;
 import peakspace.dto.response.ResponseWithGoogle;
 
-import java.io.NotActiveException;
-
 public interface UserService {
+
     ResponseWithGoogle verifyToken(String tokenFromGoogle);
 
     ResponseWithGoogle signUpWithGoogle(String tokenFromGoogle);
+
 }
