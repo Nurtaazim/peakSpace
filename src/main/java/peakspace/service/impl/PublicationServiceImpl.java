fb98e9addb17c07b0ad282936b206e11a5b25404
nurtaazim <nurtaazimmukanov24@gmail.com>
2024-04-26 13:01:59 +0600

    remove empty lines2

--- src/main/java/peakspace/service/impl/PublicationServiceImpl.java
+++ src/main/java/peakspace/service/impl/PublicationServiceImpl.java
@@ -174,7 +174,6 @@ public class PublicationServiceImpl implements PublicationService {
                 .location(publication.getLocation())
                 .commentResponses(commentForResponse)
                 .build();
-
     }
 
     private User getCurrentUser() {
@@ -184,5 +183,4 @@ public class PublicationServiceImpl implements PublicationService {
             return current;
         else throw new AccessDeniedException("Forbidden 403");
     }
-
 }
