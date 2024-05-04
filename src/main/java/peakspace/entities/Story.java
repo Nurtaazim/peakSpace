4baad49008a9302de14be192232c01b43538520b
nurtaazim <nurtaazimmukanov24@gmail.com>
2024-05-04 12:44:09 +0600

    commitForCheckout

--- src/main/java/peakspace/entities/Story.java
+++ src/main/java/peakspace/entities/Story.java
@@ -20,13 +20,10 @@ public class Story {
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH})
     private User owner;
     @ManyToMany
-<<<<<<< HEAD
     private List<User> tagFriends;
     @OneToMany(cascade = {CascadeType.PERSIST,CascadeType.DETACH})
-=======
     private List<User> tagFiends;
     @ManyToMany()
->>>>>>> 0ac0315f4a1cff4c0c42d3bfc6dea882ccb42689
     private List<Like> likes;
     @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.DETACH})
     private List<Link_Publication> linkPublications;
--- src/main/java/peakspace/repository/StoryRepository.java
+++ src/main/java/peakspace/repository/StoryRepository.java
@@ -4,11 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import peakspace.entities.Story;
 @Repository
-<<<<<<< HEAD
-public interface StoryRepository extends JpaRepository<Story, Long> {
-=======
-
 public interface StoryRepository extends JpaRepository<Story, Long> {
 
->>>>>>> 0ac0315f4a1cff4c0c42d3bfc6dea882ccb42689
+
+
 }
