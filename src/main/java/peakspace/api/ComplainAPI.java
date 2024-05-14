01b9e68bcf13268d6db213101076ff04fe7fb311
Arstanbeekovvv <arstanbeekovvv@gmail.com>
2024-05-14 13:49:10 +0600

    resolved

--- src/main/java/peakspace/api/ComplainAPI.java
+++ src/main/java/peakspace/api/ComplainAPI.java
@@ -3,14 +3,14 @@ package peakspace.api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import peakspace.dto.response.SimpleResponse;
 import peakspace.enums.Complains;
 import peakspace.service.PublicationService;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping("/complaint")
--- src/main/java/peakspace/entities/Link_PublicationRepository.java
+++
@@ -1,6 +0,0 @@
-package peakspace.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Link_PublicationRepository extends JpaRepository<Link_Publication, Long> {
-}
