e9cf2838bd27c084c80b650769eb44940a9ae728
Arstanbeekovvv <arstanbeekovvv@gmail.com>
2024-05-14 12:59:41 +0600

    chepter api done!

--- src/main/java/peakspace/api/ChapterAPI.java
+++ src/main/java/peakspace/api/ChapterAPI.java
@@ -3,8 +3,8 @@ package peakspace.api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import peakspace.service.ChapterService;
 
@@ -17,8 +17,8 @@ public class ChapterAPI {
     private final ChapterService chapterService;
 
     @Secured("USER")
-    @PostMapping("/get_chapters/{userId}")
-    @Operation(description = "Для получения всех разделов пользователя!")
+    @GetMapping("/get_chapters/{userId}")
+    @Operation(summary = "Для получения всех разделов пользователя!")
     public Map<Long, String> getAllChapter(@PathVariable Long userId){
         return chapterService.getAllChaptersByUserId(userId);
     }
