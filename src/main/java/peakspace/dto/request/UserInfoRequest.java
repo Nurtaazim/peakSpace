7ee15da352b2db16616430fad1faa8c2a6c3dd9d
nurtaazim <nurtaazimmukanov24@gmail.com>
2024-05-21 09:40:23 +0600

    refactor kyldym

--- src/main/java/peakspace/dto/request/UserInfoRequest.java
+++ src/main/java/peakspace/dto/request/UserInfoRequest.java
@@ -2,15 +2,12 @@ package peakspace.dto.request;
 
 import lombok.Getter;
 import lombok.Setter;
-import peakspace.enums.Country;
-import peakspace.validation.UniqueUserNameValidation;
 
 @Getter
 @Setter
 public class UserInfoRequest {
     private String avatar;
     private String cover;
-    @UniqueUserNameValidation
     private String userName;
     private String firstName;
     private String lastName;
