67c1e20ed1d6e96f727729ed5e3e0080062b967e
Arstanbeekovvv <arstanbeekovvv@gmail.com>
2024-06-12 18:46:13 +0600

    refactor stories

--- src/main/java/peakspace/repository/jdbsTamplate/StoryJdbcTemplate.java
+++ src/main/java/peakspace/repository/jdbsTamplate/StoryJdbcTemplate.java
@@ -1,9 +1,12 @@
 package peakspace.repository.jdbsTamplate;
 
+import peakspace.dto.response.MyStoriesResponse;
 import peakspace.dto.response.StoryAllHomPageResponse;
 
 import java.util.List;
 
 public interface StoryJdbcTemplate {
     List<StoryAllHomPageResponse> getAllFriendsStory();
+
+    List<MyStoriesResponse> getMyStories();
 }
