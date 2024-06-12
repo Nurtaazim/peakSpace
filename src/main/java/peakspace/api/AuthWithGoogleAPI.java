1491f944311584e69871f0220eec09a2b7bc4de0
Arstanbeekovvv <arstanbeekovvv@gmail.com>
2024-06-12 16:03:09 +0600

    commit

---
+++ src/main/java/peakspace/api/AuthWithGoogleAPI.java
@@ -0,0 +1,36 @@
+package peakspace.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import peakspace.dto.request.RegisterWithGoogleRequest;
+import peakspace.dto.response.ResponseWithGoogle;
+import peakspace.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth-with-google")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class AuthWithGoogleAPI {
+
+    private final UserService userService;
+
+    @PostMapping("/verify-token")
+    @Operation(description = "аутентификация через google аккаунт.")
+    public ResponseWithGoogle verifyToken(@RequestParam String tokenFromGoogle) {
+        return userService.verifyToken(tokenFromGoogle);
+    }
+
+    @PostMapping("/sign-up-with-token")
+    @Operation(description = "аутентификация и регистрация через google аккаунт.")
+    public ResponseWithGoogle signUpWithGoogle(@RequestBody RegisterWithGoogleRequest registerWithGoogle) {
+        return userService.signUpWithGoogle(registerWithGoogle);
+    }
+
+    @PostMapping("/send-again")
+    @Operation(description = "отпровить код подтверждение для регистрация через google")
+    public String sendConfCodeAgain(@RequestParam String email) throws MessagingException {
+        return userService.sendConfirmationCode(email);
+    }
+}
