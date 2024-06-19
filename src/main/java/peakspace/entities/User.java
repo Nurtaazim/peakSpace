0eeb0632df059dbf66dcde183ae7f5ed5306b12b
nurtaazim <nurtaazimmukanov24@gmail.com>
2024-06-19 20:07:48 +0600

    commit

--- src/main/java/peakspace/entities/User.java
+++ src/main/java/peakspace/entities/User.java
@@ -42,8 +42,6 @@ public class User implements UserDetails {
     private List<Chat> chats;
     @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.DETACH},fetch = FetchType.LAZY)
     private List<Comment> comments;
-//    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.DETACH})
-//    private PablicProfile pablicProfiles;
     @OneToMany(mappedBy = "owner", cascade = {CascadeType.PERSIST, CascadeType.REMOVE},fetch = FetchType.LAZY)
     private List<Publication> publications;
     @OneToMany(mappedBy = "userNotification", cascade = {CascadeType.PERSIST, CascadeType.REMOVE},fetch = FetchType.LAZY)
