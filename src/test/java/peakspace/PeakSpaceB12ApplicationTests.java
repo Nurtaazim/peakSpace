a86e0961a1e3c8c92cde455a9686e15ac64ffc11
Arstanbeekovvv <arstanbeekovvv@gmail.com>
2024-06-21 19:40:21 +0600

    push

--- src/test/java/peakspace/PeakSpaceB12ApplicationTests.java
+++ src/test/java/peakspace/PeakSpaceB12ApplicationTests.java
@@ -2,7 +2,6 @@ package peakspace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 class PeakSpaceB12ApplicationTests {
